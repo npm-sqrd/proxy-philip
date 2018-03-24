663169fdc033c6c72e7b9a9674502e62d9ce3dd9
lee-philip-n <lee.philip.n@gmail.com>
2018-03-23 19:18:04 -0700

    (feat) implemented db with SSR

--- template/script.js
+++ template/script.js
@@ -2,33 +2,30 @@ module.exports = (components, services) => `
   ${
     services.map((item) => {
       return (
-        `<div id="Summary">${item}</div>`
+        `<div id='summary'>${item}</div>`
       );
     })
   }
-
   <script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
   <script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>   
-
   ${
     Object.keys(components).map((item) => {
       const client = components[item].client;
-      const url = components[item].url;
       return (
         `<script src="${client}.js"></script>`
       );
     })
   }
-
   <script>
     ${
       Object.keys(components).map((item) => {
-        const name = components[item].name;
+        const idName = components[item].name;
         return (
-          `ReactDOM.hydrate(
-            React.createElement(${item}),
-            document.getElementById('${name}')
-          );`
+          `
+          const id = Math.floor(Math.random() * (20000000 - 10000000 + 1)) + 10000000;
+          const name = "Restaurant " + id;
+          ReactDOM.hydrate(React.createElement(${item}, { name }),document.getElementById('${idName}'));
+          `
         );
       })
     }
